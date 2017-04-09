086ecb4e3e4d9ca0441004b20a2a5fd0c95f30ad
Yuri <arkos.game.lab@gmail.com>
2017-04-09 22:11:18 +0500

    Car controls: acceleration/rotation etc

--- js/Controls.js
+++ js/Controls.js
@@ -1,7 +1,17 @@
 function SetupControls()
 {
-    car.vx = 0;
-    car.vy = 0;
+    car.anchor.x = .5;
+    car.anchor.y = .6;
+    car.x += CELL * car.anchor.x;
+    car.y += CELL * car.anchor.y;
+
+    var dAccel   = .1;
+    var dRotate  = .05;
+
+    car.velocity     = 0;
+    car.rotation     = 0;
+    car.acceleration = 0;
+    car.angle        = 0;
     
     var left  = keyboard(37),
         up    = keyboard(38),
@@ -9,38 +19,38 @@ function SetupControls()
         down  = keyboard(40);
 
     left.press = function() {
-        car.vx = -5;
+        car.angle = -dRotate;
     };
     left.release = function() {
         if (!right.isDown) {
-            car.vx = 0;
+            car.angle = 0;
         }
     };
 
     up.press = function() {
-        car.vy = -5;
+        car.acceleration = dAccel;
     };
     up.release = function() {
         if (!down.isDown) {
-            car.vy = 0;
+            car.acceleration = -dAccel;
         }
     };
 
     right.press = function() {
-        car.vx = 5;
+        car.angle = dRotate;
     };
     right.release = function() {
         if (!left.isDown) {
-            car.vx = 0;
+            car.angle = 0;
         }
     };
 
     down.press = function() {
-        car.vy = 5;
+        car.acceleration = -3*dAccel;
     };
     down.release = function() {
         if (!up.isDown) {
-            car.vy = 0;
+            car.acceleration = -dAccel;
         }
     };
 
@@ -49,6 +59,28 @@ function SetupControls()
 
 function play()
 {
-  car.x += car.vx;
-  car.y += car.vy
+    car.velocity += car.acceleration;
+    var MAXSPEED = 10;
+    if(car.velocity > MAXSPEED)
+        car.velocity = MAXSPEED;
+    if(car.velocity < 0)
+        car.velocity = 0;
+
+    car.rotation += car.angle;
+    if(car.rotation > Math.PI*2)
+        car.rotation -= Math.PI*2;
+    if(car.rotation < -Math.PI*2)
+        car.rotation += Math.PI*2;
+
+    car.x += car.velocity * Math.sin(car.rotation);
+    car.y -= car.velocity * Math.cos(car.rotation);
+
+    if(car.x > CELL*SIZEX)
+        car.x = CELL*SIZEX;
+    if(car.x < 0)
+        car.x = 0;
+    if(car.y > CELL*SIZEY)
+        car.y = CELL*SIZEY;
+    if(car.y < 0)
+        car.y = 0;
 }
