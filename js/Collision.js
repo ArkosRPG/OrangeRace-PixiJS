cff6ef9d252d4c24018692716eca79bfadd3376f
Yuri <arkos.game.lab@gmail.com>
2017-04-12 17:27:03 +0500

    format

--- js/Collision.js
+++ js/Collision.js
@@ -17,43 +17,43 @@ function CollisionCheck(car)
 
     var step = 1;
 
-for(var i = step; i <= car.velocity; i+=step)
-{
-    var x  = car.x + CARLENGTH * sin;
-    var y  = car.y - CARLENGTH * cos;
-    var dx = CARWIDTH_2 * cos;
-    var dy = CARWIDTH_2 * sin;
-    lx = (x+dx)<<0;
-    ly = (y+dy)<<0;
-    rx = (x-dx)<<0;
-    ry = (y-dy)<<0;
-
-    // collision tracking
-    // atlas required
-    if(true)
+    for(var i = step; i <= car.velocity; i+=step)
     {
-        var q = new Sprite(atlas["2px"]);
-        q.position.set(x, y);
-        carContainer.addChild(q);
-        q = new Sprite(atlas["2px"]);
-        q.position.set(lx, ly);
-        carContainer.addChild(q);
+        var x  = car.x + CARLENGTH * sin;
+        var y  = car.y - CARLENGTH * cos;
+        var dx = CARWIDTH_2 * cos;
+        var dy = CARWIDTH_2 * sin;
+        lx = (x+dx)<<0;
+        ly = (y+dy)<<0;
+        rx = (x-dx)<<0;
+        ry = (y-dy)<<0;
+    
+        // collision tracking
+        // atlas required
+        if(true)
+        {
+            var q = new Sprite(atlas["2px"]);
+            q.position.set(x, y);
+            carContainer.addChild(q);
+            q = new Sprite(atlas["2px"]);
+            q.position.set(lx, ly);
+            carContainer.addChild(q);
+        }
+    
+        if(lx<0||lx>CELL*SIZEX) return true;
+        if(ly<0||ly>CELL*SIZEY) return true;
+        if(rx<0||rx>CELL*SIZEX) return true;
+        if(ry<0||ry>CELL*SIZEY) return true;
+    
+        var dPixel = 4*(dx + dy * CELL*SIZEX);
+        var pixelL = pixelC - dPixel;
+        var pixelR = pixelC + dPixel;
+    
+        if(trackPixelData[pixelL+1] === 0) return true;
+        if(trackPixelData[pixelR+1] === 0) return true;
+        if(trackPixelData[pixelL] < 224) return true;
+        if(trackPixelData[pixelR] < 224) return true;
+        if(trackPixelData[pixelL] > 236 && trackPixelData[pixelL] < 252) return true;
+        if(trackPixelData[pixelR] > 236 && trackPixelData[pixelR] < 252) return true;
     }
-
-    if(lx<0||lx>CELL*SIZEX) return true;
-    if(ly<0||ly>CELL*SIZEY) return true;
-    if(rx<0||rx>CELL*SIZEX) return true;
-    if(ry<0||ry>CELL*SIZEY) return true;
-
-    var dPixel = 4*(dx + dy * CELL*SIZEX);
-    var pixelL = pixelC - dPixel;
-    var pixelR = pixelC + dPixel;
-
-    if(trackPixelData[pixelL+1] === 0) return true;
-    if(trackPixelData[pixelR+1] === 0) return true;
-    if(trackPixelData[pixelL] < 224) return true;
-    if(trackPixelData[pixelR] < 224) return true;
-    if(trackPixelData[pixelL] > 236 && trackPixelData[pixelL] < 252) return true;
-    if(trackPixelData[pixelR] > 236 && trackPixelData[pixelR] < 252) return true;
-}
 }
